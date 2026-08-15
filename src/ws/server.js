69664e4271662319a1e2254c8798c743431ce71f
Sarthak <messagetosarthak@gmail.com>
2026-08-15 23:13:42 +0530

    Implement fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -9,7 +9,7 @@ function sendJson(socket, payload) {
 
 function broadcast(wss, payload) {
   for (const client of wss.clients) {
-    if(client.readyState !== WebSocket.OPEN) return;
+    if(client.readyState !== WebSocket.OPEN) continue;
 
   client.send(JSON.stringify(payload));
   }
@@ -22,10 +22,21 @@ export function attachWebSocketServer(server) {
   })
 
   wss.on('connection', (socket) => {
+    socket.isAlive = true;
+    socket.on('pong', () => socket.isAlive = true)
     sendJson(socket, { type : 'welcome'})
 
     socket.on('error', console.error)
   })
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if(ws.isAlive === false) ws.terminate();
+
+      ws.isAlive = false
+      ws.ping();
+    })
+  }, 30000)
 
   function broadcastMatchCreated(match) {
     broadcast(wss, {type:'matchCreated', data:match});
